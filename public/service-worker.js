3e434befd7d7c00c3f31ab0a055b12348989c511
haywah27 <hayleywahlroos15@gmail.com>
2021-03-22 08:17:45 -0700

    starting to configure service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
-console.log("We're in the service worker file!");
+// console.log("We're in the service worker file!");
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
